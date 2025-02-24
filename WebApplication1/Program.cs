bee26b95d7f16e31a0836d5609c8bfb729c2c71f
abdulrahman <abdulrahman403868@fci.bu.edu.eg>
2025-02-24 19:05:26 +0200

    Up1

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace WebApplication1
             builder.Services.AddControllers();
             builder.Services.AddSingleton<LandmarkService>();
 
-            builder.Services.AddControllers();
+          /////
            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
